d262950f09cdab34c5189f5a93ffea431d4bdf5f
tickervoice <tickervoice@gmail.com>
2025-09-20 19:13:44 -0400

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
+function HomepageHeader(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
@@ -16,12 +16,24 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+
+        {/* Main tagline link */}
         <div style={{ marginBottom: '1rem' }}>
-				  <Link className={styles.linkText} to="/docs/readmes">
-				    {siteConfig.tagline}
-				  </Link>
-				</div>
-        
+          <Link className={styles.linkText} to="/docs/readmes">
+            {siteConfig.tagline}
+          </Link>
+        </div>
+
+        {/* New static links */}
+        <div style={{ marginBottom: '1rem', display: 'flex', gap: '1rem', flexWrap: 'wrap',justifyContent: 'center' }}>
+          <a className={styles.linkText} href="/y.html" target="_blank" rel="noreferrer">
+            yfinance
+          </a>
+          <a className={styles.linkText} href="/i.html" target="_blank" rel="noreferrer">
+            investing
+          </a>
+        </div>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
